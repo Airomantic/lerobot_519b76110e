c071206d9ce145102cac97d4b207d76c05d9037f
Alex <jiangzongqing520@gmail.com>
2025-07-17 15:20:15 +0800

    single arm conect tele ok

---
+++ src/recordDataset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python -m lerobot.record \
+    --robot.type=so101_follower \
+    --robot.port=/dev/ttyACM2 \
+    --robot.id=calibration_follower_arm \
+    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
+    --teleop.type=so101_leader \
+    --teleop.port=/dev/ttyACM1 \
+    --teleop.id=calibration_leader_arm \
+    --display_data=true \
+    --dataset.repo_id=qing/0717_1 \
+    --dataset.num_episodes=2 \
+    --dataset.single_task="Grasp a mineral water bottle and put it in the box of black" \
+    --dataset.episode_time_s=15 \
+    --dataset.reset_time_s=5 \
+    --dataset.push_to_hub=False \
+    --dataset.root=/home/qing/tianyi/code/lerobot_519b76110e/data/sampleRecord
+
+
+: <<EOF
+--dataset.root 这个路径会取代--dataset.repo_id 路径进行录制数据的存放
+
+EOF
