e098797d8200b80dcde11f2b3c27c199825a5ea9
Alex <jiangzongqing520@gmail.com>
2025-07-17 18:39:15 +0800

    fix path of local which from calibration

--- src/recordDataset.sh
+++ src/recordDataset.sh
@@ -3,11 +3,11 @@
 python -m lerobot.record \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM2 \
-    --robot.id=calibration_follower_arm \
+    --robot.id=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/robots/so101_follower/calibration_follower_arm \
     --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
     --teleop.type=so101_leader \
     --teleop.port=/dev/ttyACM1 \
-    --teleop.id=calibration_leader_arm \
+    --teleop.id=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/teleoperators/so101_leader/calibration_leader_arm \
     --display_data=true \
     --dataset.repo_id=qing/0717_1 \
     --dataset.num_episodes=2 \
@@ -20,5 +20,11 @@ python -m lerobot.record \
 
 : <<EOF
 --dataset.root 这个路径会取代--dataset.repo_id 路径进行录制数据的存放
+
+# github上推保存时
+cp -r ~/.cache/huggingface/lerobot/calibration /home/qing/tianyi/code/lerobot_519b76110e/data/
+
+# 使用校准文件时
+cp -r /home/qing/tianyi/code/lerobot_519b76110e/data/calibration ~/.cache/huggingface/lerobot
 
 EOF
