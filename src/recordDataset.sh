246f2aa9bfc7c0c374a9e3470596f703fbfe21cd
Alex <jiangzongqing520@gmail.com>
2025-07-18 21:50:22 +0800

    calib json which from dir

--- src/recordDataset.sh
+++ src/recordDataset.sh
@@ -3,11 +3,13 @@
 python -m lerobot.record \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM2 \
-    --robot.id=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/robots/so101_follower/calibration_follower_arm \
+    --robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/robots/so101_follower \
+    --robot.id=calibration_follower_arm \
     --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM1 \
-    --teleop.id=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/teleoperators/so101_leader/calibration_leader_arm \
+    --teleop.port=/dev/ttyACM3 \
+    --teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/teleoperators/so101_leader \
+    --teleop.id=calibration_leader_arm \
     --display_data=true \
     --dataset.repo_id=qing/0717_1 \
     --dataset.num_episodes=2 \
