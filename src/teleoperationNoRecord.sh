c99954c630504b69566bd4637fdfc027ab7aef4e
Alex <jiangzongqing520@gmail.com>
2025-07-22 10:14:20 +0800

    file of calibnration normal  & tele ok

--- src/teleoperationNoRecord.sh
+++ src/teleoperationNoRecord.sh
@@ -2,34 +2,38 @@
 
 python -m lerobot.teleoperate \
     --robot.type=so101_follower \
-    --robot.port=/dev/ttyACM0 \
+    --robot.port=/dev/ttyACM2 \
+    --robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
     --robot.id=my_awesome_follower_arm \
-    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
+    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 720, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM2 \
+    --teleop.port=/dev/ttyACM1 \
+    --teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
     --teleop.id=my_awesome_leader_arm \
     --display_data=true
 
 : <<EOF
 
 # 校准
-python -m lerobot.calibrate     --robot.type=so101_follower     --robot.port=/dev/ttyACM0     --robot.id=my_awesome_follower_arm
+python -m lerobot.calibrate     --robot.type=so101_follower     --robot.port=/dev/ttyACM2     --robot.id=my_awesome_follower_arm
 
 python -m lerobot.calibrate \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM2 \
-    --teleop.id=my_awesome_leader_arm
+    --teleop.port=/dev/ttyACM1 \
+    --teleop.id=my_awesome_leader_arm 
 
+cp -r ~/.cache/huggingface/lerobot/calibration /home/qing/tianyi/code/lerobot_519b76110e/utilData
+ 
 python -m lerobot.teleoperate \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM2 \
-    --robot.id=calibration_follower_arm \
     --robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
-    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
+    --robot.id=my_awesome_follower_arm \
+    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 720, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM0 \
+    --teleop.port=/dev/ttyACM1 \
     --teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
-    --teleop.id=calibration_leader_arm \
+    --teleop.id=my_awesome_leader_arm \
     --display_data=true
 
 
