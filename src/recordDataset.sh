4d20074be1b96a7d26de9ed744cb1c73c7dc14a0
Alex <jiangzongqing520@gmail.com>
2025-07-26 12:39:35 +0800

    add dual arm sh

--- src/recordDataset.sh
+++ src/recordDataset.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
+# python -m lerobot.record \
+#     --robot.type=so101_follower \
+#     --robot.port=/dev/ttyACM2 \
+#     --robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
+#     --robot.id=calib_left_follower_arm \
+#     --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
+#     --teleop.type=so101_leader \
+#     --teleop.port=/dev/ttyACM1 \
+#     --teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
+#     --teleop.id=calib_left_leader_arm \
+#     --display_data=true \
+#     --dataset.repo_id=qing/0717_1 \
+#     --dataset.num_episodes=2 \
+#     --dataset.single_task="Grasp a mineral water bottle and put it in the box of black" \
+#     --dataset.episode_time_s=15 \
+#     --dataset.reset_time_s=5 \
+#     --dataset.push_to_hub=False \
+#     --dataset.root=/home/qing/tianyi/code/lerobot_519b76110e/utilData/sampleRecord \
+#     --resume=true
+
+# 双臂
 python -m lerobot.record \
-    --robot.type=so101_follower \
-    --robot.port=/dev/ttyACM2 \
-    --robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
-    --robot.id=my_awesome_follower_arm \
-    --robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
-    --teleop.type=so101_leader \
-    --teleop.port=/dev/ttyACM1 \
-    --teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
-    --teleop.id=my_awesome_leader_arm \
-    --display_data=true \
-    --dataset.repo_id=qing/0717_1 \
+    --left_robot.type=so101_follower \
+    --left_robot.port=/dev/ttyACM5 \
+    --left_robot.id=calib_left_follower_arm \
+    --left_robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
+    --left_robot.cameras="{top: {type: opencv, index_or_path: 4, width: 1280, height: 720, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}" \
+    --right_robot.type=so101_follower \
+    --right_robot.port=/dev/ttyACM1 \
+    --right_robot.id=calib_right_follower_arm \
+    --right_robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower \
+    --right_robot.cameras="{laptop: {type: opencv, index_or_path: 8, width: 640, height: 480, fps: 15}}" \
+    --left_teleop.type=so101_leader \
+    --left_teleop.port=/dev/ttyACM0 \
+    --left_teleop.id=calib_left_leader_arm \
+    --left_teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
+    --right_teleop.type=so101_leader \
+    --right_teleop.port=/dev/ttyACM2 \
+    --right_teleop.id=calib_right_leader_arm \
+    --right_teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader \
+    --dataset.repo_id=waveHello/0719_1 \
     --dataset.num_episodes=2 \
     --dataset.single_task="Grasp a mineral water bottle and put it in the box of black" \
     --dataset.episode_time_s=15 \
     --dataset.reset_time_s=5 \
     --dataset.push_to_hub=False \
     --dataset.root=/home/qing/tianyi/code/lerobot_519b76110e/utilData/sampleRecord \
-    --resume=false
-
+    --display_data=false
 
 : <<EOF
 --dataset.root 这个路径会取代--dataset.repo_id 路径进行录制数据的存放
@@ -36,14 +64,14 @@ cp -r /home/qing/tianyi/code/lerobot_519b76110e/data/calibration ~/.cache/huggin
             "program": "${workspaceFolder}/src/lerobot/record.py",
             "args": [       
                 "--robot.type=so101_follower",
-                "--robot.port=/dev/ttyACM3",
-                "--robot.id=calibration_follower_arm",
-                "--robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/robots/so101_follower",
-                "--robot.cameras={top: {type: opencv, index_or_path: 8, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 15}}",
+                "--robot.port=/dev/ttyACM0",
+                "--robot.id=calib_left_follower_arm",
+                "--robot.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/robots/so101_follower",
+                "--robot.cameras={top: {type: opencv, index_or_path: 4, width: 1280, height: 800, fps: 15}, laptop: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 15}}",
                 "--teleop.type=so101_leader",
-                "--teleop.port=/dev/ttyACM1",
-                "--teleop.id=calibration_leader_arm",
-                "--teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/data/calibration/teleoperators/so101_leader",
+                "--teleop.port=/dev/ttyACM2",
+                "--teleop.id=calib_left_leader_arm",
+                "--teleop.calibration_dir=/home/qing/tianyi/code/lerobot_519b76110e/utilData/calibration/teleoperators/so101_leader",
                 "--display_data=true",
                 "--dataset.repo_id=waveHello/0719_1",
                 "--dataset.num_episodes=2",
